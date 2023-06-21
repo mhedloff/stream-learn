91ada53ce43cec57066855f90041055436273d7f
Maciej Hedloff <maciej.hedloff@capgemini.com>
2023-06-22 00:49:07 +0200

    diversity

--- generate_data_streams_diversity.py
+++ generate_data_streams_diversity.py
@@ -9,12 +9,13 @@ RANDOM_STATES = [1000, 100000, 101010,
                  10110, 101101, 1001,
                  10101010, 101, 110, 1337]
 
-# DDI_WEIGHTS = [(.2, .9), (.2, .7)]
-# CDI_WEIGHTS = [(2, 5, .75), (2, 4, .9)]
-CDI_WEIGHTS = [(2, 5, .75)]
-# DISCO_WEIGHTS = [(2, 5, 0.9)]
+DDI_WEIGHTS = [(.2, .9), (.2, .7)]
+CDI_WEIGHTS = [(2, 5, .75), (2, 4, .9)]
+DISCO_WEIGHTS = [(2, 5, 0.9)]
 N_CHUNKS = 100
 N_SAMPLES = 100
+# N_CHUNKS = 1000
+# N_SAMPLES = 100
 DATA_STREAMS_PATH = './final/data_streams_diversity/'
 
 
@@ -29,3 +30,10 @@ if __name__ == '__main__':
                                      n_features=N_FEATURES)
             stream_name = f"cdi__{format_cdi(cdi)}__NC_{str(N_CHUNKS)}__CS_{str(N_SAMPLES)}__RST_{str(rst)}"
             stream.save_to_npy(os.path.join(DATA_STREAMS_PATH, 'cdi/', stream_name))
+
+        for disco in DISCO_WEIGHTS:
+            stream = StreamGenerator(weights=disco, random_state=rst, n_chunks=N_CHUNKS, chunk_size=N_SAMPLES,
+                                     recurring=True, incremental=True, n_drifts=3, concept_sigmoid_spacing=5,
+                                     n_features=N_FEATURES)
+            stream_name = f"disco__{format_cdi(disco)}__NC_{str(N_CHUNKS)}__CS_{str(N_SAMPLES)}__RST_{str(rst)}"
+            stream.save_to_npy(os.path.join(DATA_STREAMS_PATH, 'disco/', stream_name))
